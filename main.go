3370ed60885b1ef27fe32701f8dda21a540f3ece
Murat TURAN <murat.turan@modanisa.com>
2021-08-29 23:09:33 +0300

    add error handling to main go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"awesomeProject/client"
 	"awesomeProject/service"
 	"flag"
+	"os"
 )
 
 func main() {
@@ -15,10 +16,23 @@ func main() {
 	contactsPath := flag.String("contactsPath", "", "contacts list for sending message")
 	flag.Parse()
 
-	if *strategyType == "instant" {
-		schedulerService.CreateInstantJob(*contactsPath)
-	} else if *strategyType == "schedule" {
-		schedulerService.CreateScheduleJob(*ordersPath)
+	if *strategyType == "instant" || *strategyType == "schedule" {
+		err := wpClient.CreateConnection()
+		if err != nil {
+			return
+		}
 	}
-
+	if *strategyType == "instant" {
+		err := schedulerService.CreateInstantJob(*contactsPath)
+		if err != nil {
+			println(err.Error())
+			os.Exit(1)
+		}
+	} else if *strategyType == "schedule" {
+		err := schedulerService.CreateScheduleJob(*ordersPath)
+		if err != nil {
+			println(err.Error())
+			os.Exit(1)
+		}
+	}
 }
