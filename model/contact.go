db37197898baf687577c05744322e11788de3097
Murat TURAN <murat.turan@modanisa.com>
2021-08-29 20:53:52 +0300

    add models for parsing csv

---
+++ model/contact.go
@@ -0,0 +1,6 @@
+package model
+
+type Contact struct {
+	Name   string
+	Number string
+}
---
+++ model/order.go
@@ -0,0 +1,8 @@
+package model
+
+type Order struct {
+	Message   string
+	Number    string
+	Date      string
+	Completed bool
+}
